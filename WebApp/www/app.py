f2b3a2004471bd026142ed1effcf9b388e06de88
Gopher <gopher0428@gmail.com>
2019-01-12 15:38:55 +0800

    Day1

--- WebApp/www/app.py
+++ WebApp/www/app.py
@@ -8,14 +8,18 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Index</h1>', content_type='text/html')
 
-@asyncio.coroutine
-def init(loop):
+def when(request):
+    now = time.strftime("%H:%M:%S", time.localtime())
+    return web.Response(body=now, content_type='text/html')
+
+async def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    app.router.add_route('GET', '/', index)     #创建router来处理浏览器请求
+    app.router.add_route('GET', '/when', when)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
-
+#协程
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
